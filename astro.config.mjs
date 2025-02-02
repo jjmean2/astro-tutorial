4fe50a05bbe97af2d15bbdb2121ad90239290cdd
Jongwan Lee <jjmean2@gmail.com>
2025-02-03 07:22:55 +0900

    Add preact component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://splendorous-sunshine-50e7a7.netlify.app/",
+  integrations: [preact()],
 });
